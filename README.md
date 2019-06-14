b7b0e709a7134ab77a73a7829d43fe9b9138065d
liugq <liugq5713@gmail.com>
2019-06-14 11:31:10 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mpa-boilerplate
+
 多页面HTML脚手架
+
+
+## Webpack
+
+`npm install webpack webpack-cli webpack-dev-server html-webpack-plugin html-loader css-loader style-loader less-loader file-loader url-loader --save-dev`
+
+使用了 less 作为css预编译引擎，方便安装
+
+## babel
+
+`npm install --save-dev @babel/core @babel/cli  babel-loader @babel/preset-env`
+
+## Eslint
+
+`npm install -g eslint` 全局安装，通过在控制台`eslint --init`初始化安装
+
+`npm install --save-dev eslint-loader`
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const devServer = require('webpack-dev-server')
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  node: {
+    fs: 'empty'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunksSortMode: 'none'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    port: 9000
+  }
+
+}
